04a83f6bb12fd216dc0234689ca515e222f1f004
年轻的 <gentryshen@icloud.com>
2021-05-31 10:50:38 +0800

    Initial Commit

---
+++ StoreKitDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StoreKitDemo
+//
+//  Created by 年轻的 on 2021/5/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StoreKitDemo/StoreKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKitDemoApp.swift
+//  StoreKitDemo
+//
+//  Created by 年轻的 on 2021/5/31.
+//
+
+import SwiftUI
+
+@main
+struct StoreKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
